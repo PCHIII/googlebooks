322bd931e757d9f230b25a5358ff318d7506cf69
PCHIII <treyhelmer@gmail.com>
2019-10-23 12:04:21 -0400

    boostrap removed, error

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-// import 'bootstrap/dist/js/bootstrap.bundle.min';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-// import Navbar from "./components/Navbar";
+// import Button from 'react-bootstrap/Button';
 
 
 
@@ -13,6 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+         {/* <Button variant="primary"> Hello </Button> */}
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -20,6 +21,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+      
         {/* <Navbar /> */}
      </div>
     );
